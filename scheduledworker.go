3b6c28be6440ba844d2b1a10375e8c97e8f40ddc
Tauki Tahmid <ttauki@gmail.com>
2022-11-29 09:02:04 +0600

    Add functionality to repeat tasks (#4)
    * add task options and repeat tasks when enabled
    
    * lock in insertTask instead of Submit

--- scheduledworker.go
+++ scheduledworker.go
@@ -10,7 +10,7 @@ const (
 )
 
 type Worker interface {
-	Submit(Task)
+	Submit(Task, ...TaskOpt)
 	SetDuration(time.Duration) Worker
 	SetMaxWorker(int) Worker
 	Start() Worker
@@ -18,14 +18,16 @@ type Worker interface {
 }
 
 type Task struct {
-	At time.Time
-	Fn func()
+	At  time.Time
+	Fn  func()
+	opt *taskOpt
 }
 
 type worker struct {
 	maxWorker int
 	tasks     []Task
 	close     chan bool
+	closed    bool
 	ticker    *time.Ticker
 	sync.Mutex
 	sync.Once
@@ -38,41 +40,39 @@ func New() Worker {
 		tasks:     make([]Task, 0),
 		close:     make(chan bool),
 		maxWorker: 10,
+		closed:    false,
 		ticker:    time.NewTicker(defaultScheduleDuration),
 	}
 }
 
-func (w *worker) Submit(task Task) {
-	w.Lock()
-	defer w.Unlock()
+func (w *worker) Submit(task Task, opts ...TaskOpt) {
+	opt := new(taskOpt)
+	for _, fn := range opts {
+		fn(opt)
+	}
+	task.opt = opt
 
-	i := 0
-	for ; i < len(w.tasks); i++ {
-		if w.tasks[i].At.After(task.At) {
-			break
+	if task.At.IsZero() {
+		task.At = time.Now()
+		if task.opt.every != 0 {
+			task.At.Add(task.opt.every)
 		}
 	}
 
-	if i == len(w.tasks) {
-		w.tasks = append(w.tasks, task)
-	} else {
-		w.tasks = append(w.tasks[:i+1], w.tasks[i:]...)
-		w.tasks[i] = task
-	}
+	w.insertTask(task)
 }
 
 func (w *worker) Start() Worker {
 
 	w.Do(func() {
 		go func() {
-			stop := false
 			for {
 				select {
 				case <-w.close:
-					stop = true
+					w.closed = true
 				case <-w.ticker.C:
 					w.process(w.getTasks())
-					if stop && len(w.tasks) == 0 {
+					if w.closed && len(w.tasks) == 0 {
 						w.ticker.Stop()
 						w.close <- true
 						return
@@ -94,6 +94,29 @@ func (w *worker) SetMaxWorker(count int) Worker {
 	defer w.Unlock()
 	w.maxWorker = count
 	return w
+}
+
+func (w *worker) insertTask(task Task) {
+	if w.closed {
+		return
+	}
+
+	w.Lock()
+	defer w.Unlock()
+
+	i := 0
+	for ; i < len(w.tasks); i++ {
+		if w.tasks[i].At.After(task.At) {
+			break
+		}
+	}
+
+	if i == len(w.tasks) {
+		w.tasks = append(w.tasks, task)
+	} else {
+		w.tasks = append(w.tasks[:i+1], w.tasks[i:]...)
+		w.tasks[i] = task
+	}
 }
 
 func (w *worker) getTasks() []Task {
@@ -126,10 +149,29 @@ func (w *worker) process(tasks []Task) {
 				defer wg.Done()
 				fn()
 			}(tasks[count].Fn)
+			w.postProcess(tasks[count])
 			count++
 		}
 		wg.Wait()
 	}
+}
+
+func (w *worker) postProcess(task Task) {
+	if task.opt.repeat != Forever && task.opt.repeat != 0 {
+		task.opt.repeat--
+	}
+
+	if task.opt.repeat == 0 {
+		return
+	}
+
+	task.At = time.Now().Add(task.opt.every)
+
+	if !task.opt.until.IsZero() && task.At.After(task.opt.until) {
+		return
+	}
+
+	w.insertTask(task)
 }
 
 func (w *worker) Stop() {
